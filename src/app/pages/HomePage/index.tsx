4f0c618d7008170ceb3727884c7a41aa6dc51485
ooiean <88550051+ooiean@users.noreply.github.com>
2022-01-02 18:27:46 +0800

    revert the changes.

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -17,7 +17,6 @@ export function HomePage() {
       </Helmet>
       <NavBar />
       <PageWrapper>
-        <h1>hello world testing from github</h1>
         <Masthead />
         <Features />
       </PageWrapper>
